2785e6fa8bc4981d978591f486334343459a6b0f
allen3_huang <allen3_huang@asus.com>
2017-10-21 13:32:19 +0800

    Initial Commit

---
+++ MultiThreadLab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultiThreadLab
+//
+//  Created by allen3_huang on 21/10/2017.
+//  Copyright © 2017 allen3_huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
